7fe0a1e1b397e804a4c4aa6da435bfd1d8097f3c
{Grant Vess} <{Grant.vess@ou.edu}>
2021-04-29 12:42:01 -0500

    screenshot uploaded, main and puzzleloader classes almost complete

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+
+	public static final String TITLE = "Grant Vess";
+	public static final boolean RESIZABLE = false;
+	public static final String STYLE_SHEET = "style.css";
+	public Stage primaryStage;
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		this.primaryStage = primaryStage;
+		PuzzleLoader puzzleLoader = new PuzzleLoader(this);
+		Scene loaderScene = new Scene(puzzleLoader);
+		
+		this.primaryStage.setScene(loaderScene);
+		this.primaryStage.setTitle(TITLE);
+		this.primaryStage.setResizable(RESIZABLE);
+		this.primaryStage.show();
+		
+	}
+	
+	public void startNonogramPlayer(Model model, int cellLength)
+	{
+		
+		NonogramView view = new NonogramView();
+		Presenter presenter = new Presenter();
+		Scene viewScene = new Scene(view);
+		viewScene.getStylesheets().add(STYLE_SHEET);
+		primaryStage.setScene(viewScene);
+		
+	}
+	
+
+}
---
+++ src/PuzzleLoader.java
@@ -0,0 +1,73 @@
+import java.io.FileNotFoundException;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+
+public class PuzzleLoader extends GridPane
+{
+	public static final int MIN_CELL_LENGTH = 20;
+	public static final int MAX_CELL_LENGTH = 50;
+	public static final int DEFAULT_CELL_LENGTH = 30;
+	public static final int SPACING = 10;
+	//will insert others
+	
+	//put name in title bar of puzzle loader
+	//screenshot save to project folder
+	
+	public PuzzleLoader(Main main)
+	{
+		Label fileLabel = new Label("Nonogram file:");
+		Label cellLabel = new Label("Cell side length (px):");
+		Spinner<Integer> cellSpinner = new Spinner<Integer>(MIN_CELL_LENGTH, MAX_CELL_LENGTH, DEFAULT_CELL_LENGTH);
+		TextField fileEntry = new TextField();
+		fileEntry.setPromptText("Enter file name");
+		Button button = new Button("Load Puzzle");
+		
+		this.add(fileLabel, 0, 0);
+		this.add(fileEntry, 1, 0);
+		this.add(cellLabel, 0, 1);
+		this.add(cellSpinner, 1, 1);
+		this.add(button, 1, 2);
+		
+		setAlignment();
+		
+		button.setOnAction(new EventHandler<ActionEvent>()
+		{
+			@Override
+			public void handle(ActionEvent event)
+			{
+				try
+				{
+					NonogramModel model = new NonogramModel();
+					main.startNonogramModel(model, cellSpinner.getValue());
+					
+					//Creates model from file
+				}
+				catch(FileNotFoundException e)
+				{
+					Alert error = new Alert(AlertType.ERROR, "File could not be read");
+					error.show();
+				}
+			}
+	
+		});
+		
+	}
+	private void setAlignment() {
+		this.setHgap(SPACING);
+		this.setVgap(SPACING);
+		this.setAlignment(Pos.CENTER);
+		this.setPadding(new Insets(SPACING, SPACING, SPACING, SPACING));
+	}
+	
+	
+}
