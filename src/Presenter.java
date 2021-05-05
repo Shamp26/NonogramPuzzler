ac83fc834ecf78e145720c857adcf0e95b5fd5c4
{Grant Vess} <{Grant.vess@ou.edu}>
2021-05-05 12:07:18 -0500

    presenter class tests complete

--- src/Presenter.java
+++ src/Presenter.java
@@ -23,6 +23,37 @@ public class Presenter
 	public void cellClicked(int rowIdx, int colIdx, boolean primaryButton)
 	{
 		
+		
+		CellState initState = model.getCellState(rowIdx, colIdx);
+		CellState chosenState;
+		// Left-clicking changes an EMPTY or MARKED cell to FILLED. Right-clicking changes an EMPTY or FILLED cell to MARKED.
+		//Left-clicking a FILLED cell or right-clicking a MARKED cell changes it to EMPTY
+		if(primaryButton == true && (initState == CellState.EMPTY || initState == CellState.MARKED))
+		{
+			chosenState = CellState.FILLED;
+			model.setCellState(rowIdx, colIdx, chosenState);
+			syncCellStates(model, view);
+			syncClueStates(model, view);
+			syncPuzzleState(model, view);
+		}
+		else if(primaryButton == false && (initState == CellState.EMPTY || initState == CellState.FILLED))
+		{
+			chosenState = CellState.MARKED;
+			model.setCellState(rowIdx, colIdx, chosenState);
+			syncCellStates(model, view);
+			syncClueStates(model, view);
+			syncPuzzleState(model, view);
+		}
+		else if((primaryButton == true && initState == CellState.FILLED) || (primaryButton == false && initState == CellState.MARKED))
+		{
+			chosenState = CellState.EMPTY;
+			model.setCellState(rowIdx, colIdx, chosenState);
+			syncCellStates(model, view);
+			syncClueStates(model, view);
+			syncPuzzleState(model, view);
+		}
+		//update model to the desired state, then update the view
+		//if that solves the puzzle, change all marked cells to empty
 	}
 
 	private void syncPuzzleState(Model model, View view) {
