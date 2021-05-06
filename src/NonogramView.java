0d239d5769a723766fc0897843e577358fc0c109
{Grant Vess} <{Grant.vess@ou.edu}>
2021-05-06 11:25:34 -0500

    Nonogram view class finished

--- src/NonogramView.java
+++ src/NonogramView.java
@@ -8,32 +8,47 @@ public class NonogramView extends BorderPane implements View
 	
 	public NonogramView(int[][] rowClues, int[][] colClues, int cellLength)
 	{
+		left = new RowCluesView(rowClues, cellLength, 1);
+		top = new ColCluesView(colClues, cellLength, 1);
+		center = new CellGridView(rowClues.length, colClues.length, cellLength);
 		
+		this.setLeft(left);
+		this.setTop(top);
+		this.setCenter(center);
+		
+		this.setStyle("nonogram-view");
 	}
 	
 	@Override
-	public void setCellState(int rowIdx, int colIdx, CellState state) {
-		// TODO Auto-generated method stub
-		
+	public void setCellState(int rowIdx, int colIdx, CellState state) 
+	{
+		center.setCellState(rowIdx, colIdx, state);
 	}
 	@Override
-	public void setRowClueState(int rowIdx, boolean solved) {
-		// TODO Auto-generated method stub
-		
+	public void setRowClueState(int rowIdx, boolean solved) 
+	{
+		left.setRowState(rowIdx, solved);
 	}
 	@Override
-	public void setColClueState(int colIdx, boolean solved) {
-		// TODO Auto-generated method stub
-		
+	public void setColClueState(int colIdx, boolean solved) 
+	{
+		top.setColState(colIdx, solved);
 	}
 	@Override
-	public void setPuzzleState(boolean solved) {
-		// TODO Auto-generated method stub
-		
+	public void setPuzzleState(boolean solved) 
+	{
+		if(solved)
+		{
+			this.setStyle("nonogram-view-solved");
+		}
+		else
+		{
+			this.setStyle("nonogram-view");
+		}
 	}
 	@Override
-	public void register(Presenter presenter) {
-		// TODO Auto-generated method stub
-		
+	public void register(Presenter presenter) 
+	{
+		this.register(presenter);
 	}
 }
